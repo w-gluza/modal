bed9ae56c8db1ae0d4239fdf9d62b0e9ebb6d918
Wioletta Gluza <wiolagluza@gmail.com>
2019-02-27 16:40:05 +0100

    modal shapes changing colours

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Modal from './components/Modal';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main>
+          <section>
+            <button onClick={this.toggleModal}>Open Modal</button>
+            {this.state.isOpen ? (
+              <Modal toggleModal={this.toggleModal} />
+            ) : null}
+          </section>
+        </main>
       </div>
     );
   }
---
+++ src/components/Modal.js
@@ -0,0 +1,131 @@
+import React from 'react';
+
+export default class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shapes: ['circle', 'square', 'triangle', 'rectangle'],
+      colors: ['#b2dfdb', '#ffe0b2', '#f8bbd0', '#dcedc8'],
+      shape: 'circle',
+      fill: '#b2dfdb'
+    };
+    // this.clearCanvas = this.clearCanvas.bind(this);
+  }
+
+  componentDidMount() {
+    const c = document.getElementById('myCanvas');
+    const ctx = c.getContext('2d');
+    const centerX = c.width / 2;
+    const centerY = c.height / 2;
+    const radius = 70;
+    let fill = this.state.fill;
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = fill;
+    ctx.fill();
+  }
+
+  componentDidUpdate() {
+    const c = document.getElementById('myCanvas');
+    const ctx = c.getContext('2d');
+    const centerX = c.width / 2;
+    const centerY = c.height / 2;
+    const radius = 70;
+    let shape = this.state.shape;
+    let fill = this.state.fill;
+
+    if (shape === 'circle') {
+      ctx.clearRect(0, 0, c.width, c.height);
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      ctx.fillStyle = fill;
+      ctx.fill();
+    } else if (shape === 'square') {
+      ctx.clearRect(0, 0, c.width, c.height);
+
+      ctx.beginPath();
+      ctx.rect(centerX - 200 / 2, centerY - 200 / 2, 200, 200);
+      ctx.fillStyle = fill;
+      ctx.fill();
+    } else if (shape === 'rectangle') {
+      ctx.clearRect(0, 0, c.width, c.height);
+
+      ctx.beginPath();
+      ctx.rect(centerX - 200 / 2, centerY - 100 / 2, 200, 100);
+      ctx.fillStyle = fill;
+      ctx.fill();
+    } else if (shape === 'triangle') {
+      ctx.clearRect(0, 0, c.width, c.height);
+
+      ctx.fillStyle = fill;
+      ctx.lineWidth = 2;
+      ctx.beginPath();
+
+      ctx.moveTo(100, 100);
+      ctx.lineTo(300, 100);
+      ctx.lineTo(100, 300);
+      ctx.lineTo(100, 100);
+
+      ctx.fill();
+      ctx.closePath();
+    } else if (shape === '' && fill === '') {
+      ctx.clearRect(0, 0, c.width, c.height);
+    }
+  }
+
+  clearCanvas() {
+    this.setState({
+      shape: '',
+      fill: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="myModal" className="modal">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2 style={{ color: this.state.fill }}>
+                Modal Header: <span>{this.state.shape}</span>
+              </h2>
+              <span className="close" onClick={this.props.toggleModal}>
+                &times; Modal Close
+              </span>
+            </div>
+            <div className="shape-list">
+              {this.state.shapes.map((shape, id) => (
+                <button
+                  key={id}
+                  onClick={() => this.setState({ shape: shape })}
+                >
+                  {shape}
+                </button>
+              ))}
+            </div>
+            <div>
+              <canvas
+                id="myCanvas"
+                className="shape-container"
+                width="300px"
+                height="300px"
+              />
+            </div>
+
+            <div className="color-list">
+              {/* <button onClick={this.clearCanvas}>clear</button> */}
+              {this.state.colors.map((color, id) => (
+                <button
+                  key={id}
+                  className="dot"
+                  onClick={() => this.setState({ fill: color })}
+                  style={{ backgroundColor: color }}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
