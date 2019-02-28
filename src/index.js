36652403392c675c4745858b437a4d4164b85846
Wioletta Gluza <wiolagluza@gmail.com>
2019-02-28 02:34:55 +0100

    dependencies cleanup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
-import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
