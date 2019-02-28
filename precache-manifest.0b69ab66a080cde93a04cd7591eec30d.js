3b7fcfa8a8d1249baa3c874c399b9975509e0f67
Wioletta Gluza <wiolagluza@gmail.com>
2019-02-28 02:22:04 +0100

    Updates

---
+++ precache-manifest.0b69ab66a080cde93a04cd7591eec30d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c47c1f6ec6c737719be",
+    "url": "/modal/static/js/runtime~main.1c47c1f6.js"
+  },
+  {
+    "revision": "ab60a7a8ec01b58ffd60",
+    "url": "/modal/static/js/main.ab60a7a8.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/modal/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "ab60a7a8ec01b58ffd60",
+    "url": "/modal/static/css/main.4ffdc2d5.chunk.css"
+  },
+  {
+    "revision": "64cfc91f22cf371b795e7e68a137b242",
+    "url": "/modal/index.html"
+  }
+];
